86af160288085f383f2e4a17a311e0aff0f02f5c
OEM Configuration (temporary user) <catchysystems2022@gmail.com>
2023-09-19 15:10:22 +0530

    code buetify

---
+++ Frontend/src/services/api_instance.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:8800/api/v1'
+const heads = { 'Content-Type': 'application/json' }
+
+if(!localStorage.getItem('accessToken')){
+  heads.Authorization = `Bearer ${localStorage.getItem('accessToken')}`
+}else{
+  heads.Authorization = `Bearer ${localStorage.getItem('accessToken')}`
+
+}
+
+const AxiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+  headers: heads
+});
+
+// Request interceptor
+AxiosInstance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response interceptor
+AxiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    // Add logic to handle response errors, e.g., token expiration
+    return Promise.reject(error);
+  }
+);
+
+export default AxiosInstance;
